1adcb6e3aefe85f5addec435d371203dca381c29
Daghan Acay <daghanacay@gmail.com>
2018-11-18 22:06:01 +1100

    final signed
    Signed-off-by: Daghan Acay <daghanacay@gmail.com>

--- src/book/createBook-handler.js
+++
@@ -1,8 +0,0 @@
-module.exports.handler = async (event, context) => {
-  return {
-    statusCode: 201,
-    body: JSON.stringify({
-      message: 'Create'
-    })
-  };
-};
--- src/hello-handler.js
+++
@@ -1,8 +0,0 @@
-module.exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello world'
-    })
-  };
-};
